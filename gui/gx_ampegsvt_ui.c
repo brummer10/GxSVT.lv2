de0457bb49fc6cf9d03c63d739cb51564be14e8b
Hermann Meyer <brummer-@web.de>
2017-05-06 12:40:15 +0200

    * reworked GUI

--- gui/gx_ampegsvt_ui.c
+++ gui/gx_ampegsvt_ui.c
@@ -82,22 +82,22 @@ static LV2UI_Handle instantiate(const LV2UI_Descriptor*   descriptor,
     gdk_color_parse("#666666", &color);
     gtk_widget_modify_fg (ui->logo, GTK_STATE_NORMAL, &color);
     GtkStyle *style = gtk_widget_get_style(ui->logo);
-    pango_font_description_set_size(style->font_desc, 16*PANGO_SCALE);
+    pango_font_description_set_size(style->font_desc, 12*PANGO_SCALE);
     pango_font_description_set_weight(style->font_desc, PANGO_WEIGHT_BOLD);
     gtk_widget_modify_font(ui->logo, style->font_desc);
 
     ui->pbox = gx_paint_box_new(GTK_ORIENTATION_VERTICAL,false, 0);
     set_expose_func(GX_AMPEGSVT_PAINT_BOX(ui->pbox),"pedal_expose");
-    ui->box = gtk_vbox_new(FALSE, 4);
-    ui->hbox = gtk_hbox_new(TRUE, 24);
-    gtk_container_set_border_width(GTK_CONTAINER(ui->pbox),5);
-    gtk_container_set_border_width(GTK_CONTAINER(ui->hbox),45);
+    ui->box = gtk_vbox_new(FALSE, 0);
+    ui->hbox = gtk_hbox_new(TRUE, 10);
+    gtk_container_set_border_width(GTK_CONTAINER(ui->pbox),35);
+    gtk_container_set_border_width(GTK_CONTAINER(ui->hbox),0);
     ui->vbox = gtk_vbox_new(TRUE, 0);
 
     gtk_box_pack_start(GTK_BOX(ui->pbox), ui->box, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(ui->box), ui->hbox, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(ui->box), ui->hbox, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(ui->box), ui->vbox, TRUE, TRUE, 0);
-    gtk_box_pack_end(GTK_BOX(ui->pbox), ui->logo, FALSE, FALSE, 0);
+    //gtk_box_pack_end(GTK_BOX(ui->pbox), ui->logo, FALSE, FALSE, 0);
     gdk_color_parse("#888888", &color);
 
 
@@ -113,6 +113,7 @@ static LV2UI_Handle instantiate(const LV2UI_Descriptor*   descriptor,
     gtk_widget_modify_font(ui->bp_label, style->font_desc);
 
     gtk_box_pack_start(GTK_BOX(ui->hbox), ui->bp_box, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(ui->bp_box), ui->logo, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(ui->bp_box), ui->bypass, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(ui->bp_box), ui->bp_label, FALSE, FALSE, 0);
     ui->bp_args = (struct gx_args*) malloc(sizeof(struct gx_args));
@@ -181,6 +182,26 @@ static LV2UI_Handle instantiate(const LV2UI_Descriptor*   descriptor,
     ui->args[6]->port_index = (int)HIGHSWITCH;
     g_signal_connect(G_OBJECT(ui->adj[6]), "value-changed",
           G_CALLBACK(ref_value_changed),(gpointer*)ui->args[6]);
+
+    ui->adj[7] = gtk_adjustment_new( 0.0, 0.0, 1.0, 1.0, 0.01*10.0, 0);
+    ui->knob[7] = gtk_switch_new_with_adjustment(GTK_ADJUSTMENT(ui->adj[7]));
+    ui->label[7] = gtk_label_new("CAB");
+    ui->vkbox[7] = gtk_vbox_new(FALSE, 0);
+
+    gtk_widget_modify_fg (ui->label[7], GTK_STATE_NORMAL, &color);
+    style = gtk_widget_get_style(ui->label[7]);
+    pango_font_description_set_size(style->font_desc, 10*PANGO_SCALE);
+    pango_font_description_set_weight(style->font_desc, PANGO_WEIGHT_BOLD);
+    gtk_widget_modify_font(ui->label[7], style->font_desc);
+
+    gtk_box_pack_start(GTK_BOX(ui->hbox), ui->vkbox[7], TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(ui->vkbox[7]), ui->knob[7], TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(ui->vkbox[7]), ui->label[7], FALSE, FALSE, 0);
+    ui->args[7] = (struct gx_args*) malloc(sizeof(struct gx_args));
+    ui->args[7]->ui = ui;
+    ui->args[7]->port_index = (int)CABSWITCH;
+    g_signal_connect(G_OBJECT(ui->adj[7]), "value-changed",
+          G_CALLBACK(ref_value_changed),(gpointer*)ui->args[7]);
 
     ui->adj[0] = gtk_adjustment_new( 0.5, 0.0, 1.0, 0.01, 0.01*10.0, 0);
     ui->knob[0] = gtk_knob_new_with_adjustment(GTK_ADJUSTMENT(ui->adj[0]));
@@ -261,26 +282,6 @@ static LV2UI_Handle instantiate(const LV2UI_Descriptor*   descriptor,
     ui->args[3]->port_index = (int)VOLUME;
     g_signal_connect(G_OBJECT(ui->adj[3]), "value-changed",
           G_CALLBACK(ref_value_changed),(gpointer*)ui->args[3]);
-
-    ui->adj[7] = gtk_adjustment_new( 0.0, 0.0, 1.0, 1.0, 0.01*10.0, 0);
-    ui->knob[7] = gtk_switch_new_with_adjustment(GTK_ADJUSTMENT(ui->adj[7]));
-    ui->label[7] = gtk_label_new("CAB");
-    ui->vkbox[7] = gtk_vbox_new(FALSE, 0);
-
-    gtk_widget_modify_fg (ui->label[7], GTK_STATE_NORMAL, &color);
-    style = gtk_widget_get_style(ui->label[7]);
-    pango_font_description_set_size(style->font_desc, 10*PANGO_SCALE);
-    pango_font_description_set_weight(style->font_desc, PANGO_WEIGHT_BOLD);
-    gtk_widget_modify_font(ui->label[7], style->font_desc);
-
-    gtk_box_pack_start(GTK_BOX(ui->hbox), ui->vkbox[7], TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(ui->vkbox[7]), ui->knob[7], TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(ui->vkbox[7]), ui->label[7], FALSE, FALSE, 0);
-    ui->args[7] = (struct gx_args*) malloc(sizeof(struct gx_args));
-    ui->args[7]->ui = ui;
-    ui->args[7]->port_index = (int)CABSWITCH;
-    g_signal_connect(G_OBJECT(ui->adj[7]), "value-changed",
-          G_CALLBACK(ref_value_changed),(gpointer*)ui->args[7]);
 
 
     *widget = ui->pbox;
