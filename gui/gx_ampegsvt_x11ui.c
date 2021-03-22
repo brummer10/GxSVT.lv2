86bf0011d63be3a0fb3165dac37e006deab5c2ef
brummer10 <brummer-@web.de>
2021-03-22 19:24:12 +0100

    Fix issue #33 ON/OFF toggle button doesn't work on several plugins

--- gui/gx_ampegsvt_x11ui.c
+++ gui/gx_ampegsvt_x11ui.c
@@ -656,7 +656,6 @@ static void check_value_changed(gx_ampegsvtUI *ui, int i, float* value) {
 		if (ui->block_event != ui->controls[i].port)
 			ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
 		send_controller_event(ui, i);
-		ui->block_event = -1;
 	}
 }
 
@@ -1021,6 +1020,7 @@ static void port_event(LV2UI_Handle handle, uint32_t port_index,
 		if (port_index == ui->controls[i].port) {
 			ui->block_event = (int)port_index;
 			check_value_changed(ui, i, &value);
+			ui->block_event = -1;
 		}
 	}
 }
